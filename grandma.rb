4f4daa6ce57c7563dc56718910851155496d1491
Irum F <irummf@gmail.com>
2018-08-30 08:26:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end 
 end 
