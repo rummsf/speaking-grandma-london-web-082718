d480da6ae4659f4ccfdde229bacd06f1fe64cffc
Irum F <irummf@gmail.com>
2018-08-30 08:27:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase.downcase 
-    puts "HUH?! SPEAK UP, SONNY!"
+    print "HUH?! SPEAK UP, SONNY!"
   elsif phrase.capitalize
-    puts "HUH?! SPEAK UP, SONNY!"
+    print "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    print "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end 
